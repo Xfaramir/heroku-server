0adc64cf6aab2dc7e53db33ba48cf7ae5b6addbd
xfaramir <xfaramir@gmail.com>
2018-10-17 15:54:43 -0500

    Added Config File and MiddleWares

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
+require('./config/config')
+
 const express = require('express');
-const app= express();
 const bodyParser= require('body-parser');
 const morgan = require('morgan');
-
+const session = require('cookie-session');
+const port = process.env.PORT;
+const app= express();
 
 //MiddleWare
 app.use(bodyParser.urlencoded({
@@ -11,3 +14,6 @@ app.use(bodyParser.urlencoded({
 
 app.use(morgan('dev'));
 
+app.listen(port,function () {
+    console.log("Listening on port: ",port);
+})
